c43ecf399faf959ca5ee4f189520a938655b1dbd
Ken Spencer <me@iotaspencer.me>
2018-11-26 23:56:34 -0500

    rename api_tree.rb -> tree.rb

--- lib/cloudflare_cli/api_tree.rb
+++
@@ -1,5 +0,0 @@
-require 'httparty'
-module CloudflareCli
-  #autoload :Zones, 'cloudflare_cli/tree/zones'
-  autoload :IPs, 'cloudflare_cli/tree/ips'
-end
---
+++ lib/cloudflare_cli/tree.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+
+module CloudflareCli
+  module Tree
+    #autoload :Zones, 'cloudflare_cli/tree/zones'
+    autoload :IPs, 'cloudflare_cli/tree/ips'
+  end
+end
