3b4c2d0296c5520a1d3c9494d353d890afc52f26
Ken Spencer <me@iotaspencer.me>
2018-11-29 09:40:28 -0500

    put zones commands into their own file

---
+++ lib/cloudflare_cli/commands/003_zones.rb
@@ -0,0 +1,20 @@
+module CloudflareCli
+  class App
+    extend GLI::App
+
+    desc "Use all the non-dns '/zones/*' endpoints"
+    command :zones do |c|
+      c.desc "list all zones or filter them"
+      c.command [:all, "list-zones"] do |lz|
+        lz.example "#{exe_name} zones [list-zones|all]", desc: "List all zones without filtering them"
+        lz.flag [:name], desc: "Zone name to search for"
+        lz.action do |global_options, options, args|
+          puts CloudflareCli::Endpoints::Zones.all(options)
+        end
+      end
+      c.action do |global_options, options, args|
+      end
+      c.default_command false
+    end
+  end
+end
