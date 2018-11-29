cc8d25dc67d4854a6f8d72e251765abe29716b64
Ken Spencer <me@iotaspencer.me>
2018-11-29 09:41:10 -0500

    move zones to their own file

--- lib/cloudflare_cli/app.rb
+++ lib/cloudflare_cli/app.rb
@@ -28,25 +28,6 @@ module CloudflareCli
     flag [:email], desc: "Cloudflare E-Mail", default_value: "", arg_name: "EMAIL", mask: true
     flag ["api-key"], desc: "Cloudflare API Key", default_value: "", arg_name: "APIKEY", mask: true
 
-
-    desc "Use all the non-dns '/zones/*' endpoints"
-    command :zones do |c|
-      c.desc "list all zones or filter them"
-      c.command [:all, "list-zones"] do |lz|
-        lz.example "#{exe_name} zones [list-zones|all]", desc: "List all zones without filtering them"
-        lz.flag [:name], desc: "Zone name to search for"
-        lz.action do |global_options, options, args|
-          puts CloudflareCli::Endpoints::Zones.all(options)
-        end
-      end
-      c.action do |global_options, options, args|
-      end
-      c.default_command false
-    end
-
-
-
-
     around do |global_options, command, options, arguments, code|
       CloudflareCli::State.initiate(global_options)
       code.call
