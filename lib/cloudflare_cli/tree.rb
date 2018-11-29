b66104d1b7cf348cfefc4ffc2360dc0ee2ac29d8
Ken Spencer <me@iotaspencer.me>
2018-11-28 22:54:35 -0500

    use tree as a middle module to incorporate endpoints as we make them

--- lib/cloudflare_cli/tree.rb
+++ lib/cloudflare_cli/tree.rb
@@ -1,8 +1,9 @@
-require 'httparty'
+require 'cloudflare_cli/tree/ips'
+require 'cloudflare_cli/tree/zones'
 
 module CloudflareCli
   module Tree
     #autoload :Zones, 'cloudflare_cli/tree/zones'
-    autoload :IPs, 'cloudflare_cli/tree/ips'
+
   end
 end
