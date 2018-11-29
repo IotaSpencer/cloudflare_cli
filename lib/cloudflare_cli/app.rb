ffa85c8d872a7ebecb2089470e3231e5f6c726b7
Ken Spencer <me@iotaspencer.me>
2018-11-29 01:28:48 -0500

    clean up app and load from command files to make it cleaner to work on

--- lib/cloudflare_cli/app.rb
+++ lib/cloudflare_cli/app.rb
@@ -23,54 +23,13 @@ module CloudflareCli
     arguments :strict
     wrap_help_text :verbatim
 
-    flag [:email], desc: "Cloudflare E-Mail", default_value: "", arg_name: "EMAIL"
-    flag ["api-key"], desc: "Cloudflare API Key", default_value: "", arg_name: "APIKEY"
 
-    desc "Describe user here"
-    arg_name "Describe arguments to user here"
-    command :user do |c|
-      c.desc "Describe a switch to user"
-      c.switch :s
+    commands_from 'cloudflare_cli/commands'
+    flag [:email], desc: "Cloudflare E-Mail", default_value: "", arg_name: "EMAIL", mask: true
+    flag ["api-key"], desc: "Cloudflare API Key", default_value: "", arg_name: "APIKEY", mask: true
 
-      c.desc "Describe a flag to user"
-      c.default_value "default"
-      c.flag :f
-      c.action do |global_options, options, args|
 
-        # Your command logic here
-
-        # If you have any errors, just raise them
-        # raise "that command made no sense"
-
-        puts "user command ran"
-      end
-    end
-
-    desc "Describe account here"
-    arg_name "Describe arguments to account here"
-    command :account do |c|
-      c.action do |global_options, options, args|
-        puts "account command ran"
-      end
-    end
-
-    desc "Describe orgs here"
-    arg_name "Describe arguments to orgs here"
-    command :orgs do |c|
-      c.action do |global_options, options, args|
-        puts "orgs command ran"
-      end
-    end
-
-    desc "Describe invites here"
-    arg_name "Describe arguments to invites here"
-    command :invites do |c|
-      c.action do |global_options, options, args|
-        puts "invites command ran"
-      end
-    end
-
-    desc "Use the '/zones/*' endpoints"
+    desc "Use all the non-dns '/zones/*' endpoints"
     command :zones do |c|
       c.desc "list all zones or filter them"
       c.command [:all, "list-zones"] do |lz|
@@ -85,44 +44,9 @@ module CloudflareCli
       c.default_command false
     end
 
-    desc "Describe dns here"
-    arg_name "Describe arguments to dns here"
-    command :dns do |c|
-      c.action do |global_options, options, args|
-        puts "dns command ran"
-      end
-    end
 
-    desc "Describe vdns here"
-    arg_name "Describe arguments to vdns here"
-    command :vdns do |c|
-      c.action do |global_options, options, args|
-        puts "vdns command ran"
-      end
-    end
 
-    desc "Return Cloudflare IPs"
-    command :ips do |c|
-      c.command :list do |li|
-        li.switch [:pretty, :p], desc: "make the display of the ips pretty", negatable: false
 
-        li.action do |global_options, options, args|
-          if options[:pretty]
-            pp CloudflareCli::Endpoints::IPs.list.body
-          else
-            puts CloudflareCli::Endpoints::IPs.list.body
-          end
-        end
-      end
-    end
-
-    desc "Describe ssl here"
-    arg_name "Describe arguments to ssl here"
-    command :ssl do |c|
-      c.action do |global_options, options, args|
-        puts "ssl command ran"
-      end
-    end
     around do |global_options, command, options, arguments, code|
       CloudflareCli::State.initiate(global_options)
       code.call
