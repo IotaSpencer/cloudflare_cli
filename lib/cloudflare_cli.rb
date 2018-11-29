123d079b3d42ae4784e5d5f258502e20a1fa46ae
Ken Spencer <me@iotaspencer.me>
2018-11-28 22:52:18 -0500

    allow state initialization without a class having to be made and saved

--- lib/cloudflare_cli.rb
+++ lib/cloudflare_cli.rb
@@ -1,4 +1,20 @@
-require 'cloudflare_cli/version'
-require 'cloudflare_cli/app'
+require "rubyflare"
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+
+module CloudflareCli
+  class State
+    # @param [Hash] options Hash of options to initiate the State
+    def self.initiate(options)
+      @@email = options[:email]
+      @@api_key = options["api-key"]
+      @@ctx = Rubyflare.connect_with(@@email, @@api_key)
+    end
+    def self.ctx
+      @@ctx
+    end
+  end
+end
+
+require "cloudflare_cli/version"
+require "cloudflare_cli/app"
