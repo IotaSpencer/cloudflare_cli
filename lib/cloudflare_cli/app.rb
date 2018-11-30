216ce7bf9b0b5a3f51ec74d45e4d544598900ddc
Ken Spencer <me@iotaspencer.me>
2018-11-30 09:41:10 -0500

    use single quotes/apostrophes instead of quotations/double quotes

--- lib/cloudflare_cli/app.rb
+++ lib/cloudflare_cli/app.rb
@@ -1,32 +1,32 @@
-require "gli"
-require "json"
-require "cloudflare_cli/tree"
+require 'gli'
+require 'json'
+require 'cloudflare_cli/tree'
 
 module CloudflareCli
+  # Have a class for the GLI space
   class App
     extend GLI::App
-    program_desc "Interface with the Cloudflare API!"
+    program_desc 'Interface with the Cloudflare API!'
 
-    program_long_desc <<~HEREDOC
+    program_long_desc <<-HEREDOC
 
-                        #{exe_name} is an interface into the Cloudflare API,
+    #{exe_name} is an interface into the Cloudflare API,
     usage is rate-limited by Cloudflare itself, not the gem.
 
     See https://api.cloudflare.com/#getting-started-requests for information
     on the API and the linked anchor for rate-limiting specifically.
-                      HEREDOC
+    HEREDOC
 
     version CloudflareCli::VERSION
 
-    config_file ".cfcli/config"
+    config_file '.cfcli/config'
     subcommand_option_handling :normal
     arguments :strict
     wrap_help_text :verbatim
 
-
     commands_from 'cloudflare_cli/commands'
-    flag [:email], desc: "Cloudflare E-Mail", default_value: "", arg_name: "EMAIL", mask: true
-    flag ["api-key"], desc: "Cloudflare API Key", default_value: "", arg_name: "APIKEY", mask: true
+    flag [:email], desc: 'Cloudflare E-Mail', default_value: '', arg_name: 'EMAIL', mask: true
+    flag ['api-key'], desc: 'Cloudflare API Key', default_value: '', arg_name: 'APIKEY', mask: true
 
     around do |global_options, command, options, arguments, code|
       CloudflareCli::State.initiate(global_options)
