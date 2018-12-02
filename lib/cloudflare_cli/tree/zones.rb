169757810e31df4b49148e1bbd23633d43642649
Ken Spencer <me@iotaspencer.me>
2018-12-02 00:35:26 -0500

    redo how methods are declared to implement method based rescuing

--- lib/cloudflare_cli/tree/zones.rb
+++ lib/cloudflare_cli/tree/zones.rb
@@ -5,16 +5,13 @@ module CloudflareCli
     module Zone
       class Zones
       # Return Cloudflare Account Zones
-      def self.all(options)
-        puts options
-        #begin
-        #  ctx = CloudflareCli::State.ctx
-        #  ctx.get('zones')
-        #rescue Rubyflare::ConnectionError => e
-        #  puts e.response.body
-        #end
+        def self.all(options)
+          ctx = CloudflareCli::State.ctx
+          ctx.get('zones', options)
+        rescue Rubyflare::ConnectionError => e
+          puts e.response.body
+        end
       end
-    end
     end
   end
 end
