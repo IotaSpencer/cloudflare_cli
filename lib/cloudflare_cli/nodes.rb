3986208a3eaa74bf597ce6929f2454943f032eb1
Ken Spencer <me@iotaspencer.me>
2018-12-08 10:56:53 -0500

    move tree to nodes and autoload Modules or classes as we need them

---
+++ lib/cloudflare_cli/nodes.rb
@@ -0,0 +1,10 @@
+
+module CloudflareCli
+  module Nodes
+    autoload :IPs, 'cloudflare_cli/nodes/ips'
+    autoload :Zone, 'cloudflare_cli/nodes/zones'
+    autoload :Accounts, 'cloudflare_cli/nodes/accounts'
+    autoload :Memberships, 'cloudflare_cli/nodes/memberships'
+
+  end
+end
--- lib/cloudflare_cli/tree.rb
+++
@@ -1,9 +0,0 @@
-require 'cloudflare_cli/tree/ips'
-require 'cloudflare_cli/tree/zones'
-
-module CloudflareCli
-  module Tree
-    #autoload :Zones, 'cloudflare_cli/tree/zones'
-
-  end
-end
